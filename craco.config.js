c14e6053ab24abed41c1ff3e4002f2cb42d0ae0c
hungphongtruong <programmingd32@gmail.com>
2021-12-03 11:57:55 +0700

    compress image

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,5 @@
 const CracoAlias = require("craco-alias");
+const imageOptimizer = require("craco-image-optimizer-plugin");
 
 module.exports = {
   plugins: [
@@ -8,6 +9,31 @@ module.exports = {
         source: "tsconfig",
         /* tsConfigPath should point to the file where "paths" are specified */
         tsConfigPath: "./tsconfig.paths.json",
+      },
+    },
+    {
+      plugin: imageOptimizer,
+      // image-webpack-plugin options
+      options: {
+        mozjpeg: {
+          progressive: true,
+          quality: 65,
+        },
+        // optipng.enabled: false will disable optipng
+        optipng: {
+          enabled: false,
+        },
+        pngquant: {
+          quality: [0.65, 0.9],
+          speed: 4,
+        },
+        gifsicle: {
+          interlaced: false,
+        },
+        // the webp option will enable WEBP
+        webp: {
+          quality: 75,
+        },
       },
     },
   ],
