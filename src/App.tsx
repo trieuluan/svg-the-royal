d44b55d9611073f5920b80e9b39d9ce893f3ef93
hungphongtruong <programmingd32@gmail.com>
2021-12-06 11:13:37 +0700

    adapt new slide

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import SecondSlide from "./components/SecondSlide";
 import WebFont from "webfontloader";
 import { domAnimation, LazyMotion } from "framer-motion";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import FirstSlide from "./components/FirstSlide";
 
 function App() {
@@ -15,12 +15,12 @@ function App() {
   }, []);
   return (
     <LazyMotion features={domAnimation}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path={"1"} element={<SecondSlide />} />
           <Route path={"2"} element={<FirstSlide />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </LazyMotion>
   );
 }
