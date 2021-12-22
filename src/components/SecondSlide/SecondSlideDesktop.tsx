953f288afa1c21b6294a4b0852c53905d11fe7f7
hungphongtruong <programmingd32@gmail.com>
2021-12-22 07:34:10 +0700

    update background

--- src/components/SecondSlide/SecondSlideDesktop.tsx
+++ src/components/SecondSlide/SecondSlideDesktop.tsx
@@ -1,6 +1,5 @@
 import background from "./Slide2.png";
-import { Box } from "@mui/material";
-import { sxFullSize } from "@hungphongbk/vth-sdk/utils/predefinedSx";
+import { Box, styled } from "@mui/material";
 import FirstLabel from "./FirstLabel";
 import FirstLine from "./FirstLine";
 import SecondLabel from "./SecondLabel";
@@ -29,59 +28,82 @@ export const tlwh = (y: number, x: number, w: number, h: number) => ({
   height: getYSize(h),
 });
 
+const Bg = styled("img")`
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+  object-position: bottom;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`;
+
 type SecondSlideProps = unknown;
 export default function SecondSlideDesktop(
   props: SecondSlideProps
 ): JSX.Element {
   const [content, setContent] = useState("");
   return (
-    <AspectRatio
-      ratio={"1919.84/929.27"}
+    <Box
       sx={{
-        "& img": {
-          ...sxFullSize,
-          top: 0,
-          left: 0,
-          objectFit: "cover",
-        },
+        width: "100vw",
+        height: "100vh",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        overflow: "hidden",
       }}
     >
-      <Box
-        sx={{
-          "& >*": { position: "absolute" },
-          "& .pointable": {
-            cursor: "pointer",
-          },
-        }}
-      >
-        <img src={background} />
-        <SlideLabel sx={tlwh(55.78, 179.42, 611.88, 134.67)} />
-        <SlideContext.Provider value={[content, setContent]}>
-          <FirstLine
+      <Box sx={{ bottom: 0, right: 0, left: 0, position: "absolute" }}>
+        <Bg src={background} />
+        <AspectRatio
+          ratio={"1919.84/929.27"}
+          sx={{
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            right: 0,
+          }}
+        >
+          <Box
             sx={{
-              ...tlwh(235.54, 822.26, 434.31, 27.96),
+              "& >*": { position: "absolute" },
+              "& .pointable": {
+                cursor: "pointer",
+              },
             }}
-          />
-          <FirstLabel sx={tlwh(228, 179, 651.81, 71)} />
+          >
+            <SlideLabel sx={tlwh(55.78, 179.42, 611.88, 134.67)} />
+            <SlideContext.Provider value={[content, setContent]}>
+              <FirstLine
+                sx={{
+                  ...tlwh(235.54, 822.26, 434.31, 27.96),
+                }}
+              />
+              <FirstLabel sx={tlwh(228, 179, 651.81, 71)} />
 
-          <SecondLine sx={{ ...tlwh(309.27, 826.4, 430.17, 47) }} />
-          <SecondLabel sx={tlwh(320, 179, 651.81, 71)} />
+              <SecondLine sx={{ ...tlwh(309.27, 826.4, 430.17, 47) }} />
+              <SecondLabel sx={tlwh(320, 179, 651.81, 71)} />
 
-          <ThirdLine sx={tlwh(446.69, 828.23, 428.34, 130.85)} />
-          <ThirdLabel sx={tlwh(412, 179, 651.81, 71)} />
+              <ThirdLine sx={tlwh(446.69, 828.23, 428.34, 130.85)} />
+              <ThirdLabel sx={tlwh(412, 179, 651.81, 71)} />
 
-          <FourthLine sx={tlwh(540.5, 824.12, 437.45, 202.04)} />
-          <FourthLabel sx={tlwh(507, 179, 651.81, 71)} />
+              <FourthLine sx={tlwh(540.5, 824.12, 437.45, 202.04)} />
+              <FourthLabel sx={tlwh(507, 179, 651.81, 71)} />
 
-          <FifthLine sx={tlwh(635.81, 826.78, 435.79, 156.72)} />
-          <FifthLabel sx={tlwh(601, 179, 651.81, 71)} />
+              <FifthLine sx={tlwh(635.81, 826.78, 435.79, 156.72)} />
+              <FifthLabel sx={tlwh(601, 179, 651.81, 71)} />
 
-          <SixthLine sx={tlwh(730.79, 829, 427.57, 116.75)} />
-          <SixthLabel sx={tlwh(696, 179, 651.81, 71)} />
+              <SixthLine sx={tlwh(730.79, 829, 427.57, 116.75)} />
+              <SixthLabel sx={tlwh(696, 179, 651.81, 71)} />
 
-          <BuildingAreas sx={tlwh(109.28, 1260.45, 361.49, 805.33)} />
-        </SlideContext.Provider>
+              <BuildingAreas sx={tlwh(109.28, 1260.45, 361.49, 805.33)} />
+            </SlideContext.Provider>
+          </Box>
+        </AspectRatio>
       </Box>
-    </AspectRatio>
+    </Box>
   );
 }
