924669920df3a5aacdcfd7019fd0b88f58dea732
trieuluan <chaybo2006@gmail.com>
2022-03-29 16:05:02 +0700

    Updates

---
+++ static/js/22.883e7809.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsvg-the-royal"]=this["webpackJsonpsvg-the-royal"]||[]).push([[22],{214:function(t,n,e){"use strict";e.r(n),e.d(n,"default",(function(){return s}));var r=e(4);function s(t){return Object(r.jsx)(r.Fragment,{})}}}]);
+//# sourceMappingURL=22.883e7809.chunk.js.map
