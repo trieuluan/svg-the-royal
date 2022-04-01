a57f6f91fef8a9c08ce9dc2be988dab9688fa8b2
hanna27 <74099553+thamhangruri@users.noreply.github.com>
2022-04-01 18:04:34 +0700

    Updates

---
+++ static/js/19.0e73c15c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsvg-the-royal"]=this["webpackJsonpsvg-the-royal"]||[]).push([[19],{202:function(t,n,e){"use strict";e.r(n),e.d(n,"default",(function(){return s}));var r=e(3);function s(t){return Object(r.jsx)(r.Fragment,{})}}}]);
+//# sourceMappingURL=19.0e73c15c.chunk.js.map
