09ac4f801390ff38e582930d3cd563d04ea4e207
Harden <harden@digityze.asia>
2022-12-01 10:27:16 +0700

    Updates

---
+++ static/js/27.4597a5fa.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsvg-the-royal"]=this["webpackJsonpsvg-the-royal"]||[]).push([[27],{211:function(t,n,e){"use strict";e.r(n),e.d(n,"default",(function(){return s}));var r=e(3);function s(t){return Object(r.jsx)(r.Fragment,{})}}}]);
+//# sourceMappingURL=27.4597a5fa.chunk.js.map
---
+++ static/js/28.dc4c3734.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsvg-the-royal"]=this["webpackJsonpsvg-the-royal"]||[]).push([[28],{209:function(t,n,e){"use strict";e.r(n),e.d(n,"default",(function(){return s}));var r=e(3);function s(t){return Object(r.jsx)(r.Fragment,{})}}}]);
+//# sourceMappingURL=28.dc4c3734.chunk.js.map
